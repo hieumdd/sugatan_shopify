52cf261f6d81800f278234af12ac9685a14a24e8
HM <hieumdd@gmail.com>
2021-11-14 20:50:37 +0700

    feat: Added SniffAndBark

--- test/test_manual.py
+++ test/test_manual.py
@@ -2,10 +2,10 @@ from datetime import datetime
 
 from .utils import process
 
-CLIENT_NAME = "MotivArt"
-SHOP_URL = "willyoudo.myshopify.com"
-START = datetime(2021, 8, 15).strftime("%Y-%m-%dT%H:%M:%S%z")
-END = datetime(2021, 8, 30).strftime("%Y-%m-%dT%H:%M:%S%z")
+CLIENT_NAME = "SniffAndBark"
+SHOP_URL = "sniffandbark.myshopify.com"
+START = datetime(2021, 10, 1).strftime("%Y-%m-%dT%H:%M:%S%z")
+END = datetime(2021, 11, 1).strftime("%Y-%m-%dT%H:%M:%S%z")
 
 def test_manual():
     """Test the scripts for manual mode"""    
